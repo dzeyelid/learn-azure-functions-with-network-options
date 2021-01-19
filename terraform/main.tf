3ea56f0b0e4acabb0990a60d47209c8cb7cd3015
Kazumi OHIRA (@dz_) <dzeyelid@gmail.com>
2021-01-19 19:17:25 +0900

    Set function package url to WEBSITE_RUN_FROM_PACKAGE (#5)

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,22 +11,31 @@ provider "azurerm" {
   features {}
 }
 
-module "service_endpint" {
+module "get_function_package_url" {
+  source = "./modules/get_function_package_url"
+
+  asset_name = "functions.zip"
+}
+
+module "service_endpoint" {
   source = "./modules/service_endpoint"
 
-  identifier = "${var.identifier}-se"
-  location   = "japaneast"
+  identifier           = "${var.identifier}-se"
+  location             = "japaneast"
+  function_package_url = module.get_function_package_url.download_url
 }
 
-module "service_endpint_west_eu" {
+module "service_endpoint_west_eu" {
   source = "./modules/service_endpoint_west_eu"
 
-  identifier = "${var.identifier}-sewe"
+  identifier           = "${var.identifier}-sewe"
+  function_package_url = module.get_function_package_url.download_url
 }
 
-module "private_endpint" {
+module "private_endpoint" {
   source = "./modules/private_endpoint"
 
-  identifier = "${var.identifier}-pe"
-  location   = "japaneast"
+  identifier           = "${var.identifier}-pe"
+  location             = "japaneast"
+  function_package_url = module.get_function_package_url.download_url
 }
--- terraform/modules/service_endpoint_west_eu/variables.tf
+++ terraform/modules/service_endpoint_west_eu/variables.tf
@@ -1,3 +1,7 @@
 variable "identifier" {
   type = string
 }
+
+variable "function_package_url" {
+  type = string
+}
